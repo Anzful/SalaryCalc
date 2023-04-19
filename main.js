3025634670cc0166854da2741d74e3739cbe1b49
Anzo <2003anzori@gmail.com>
2023-04-19 14:55:20 +0400

    rqred

---
+++ main.js
@@ -0,0 +1,5 @@
+const working_hours = document.getElementById('working_hours');
+const sgc = document.getElementById('sgc').value;
+const reaction_time = document.getElementById('reaction_time');
+const pa = document.getElementById('pa');
+const game_speed = document.getElementById('game_speed');
