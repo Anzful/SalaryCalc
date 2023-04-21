22e5e0c4ad0783ef0fb009ee8a2253ad4fa0f4f7
Anzo <2003anzori@gmail.com>
2023-04-21 22:47:51 +0400

    **

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const working_hours = document.getElementById('working_hours');
-const sgc = document.getElementById('sgc').value;
+const sgc = document.getElementById('sgc');
 const reaction_time = document.getElementById('reaction_time');
 const pa = document.getElementById('pa');
 const game_speed = document.getElementById('game_speed');
+const button = document.getElementById('button');
+const stage = document.getElementById('stage');
+const shift = document.getElementById('shift');
+
+button.addEventListener('click', (ev)=>{
+    
+    alert(parseInt(working_hours.value))
+
+})
+
+
+
+
+
+
+
+
+
+function handlesubmit(ev){
+    ev.preventDefault();
+}
