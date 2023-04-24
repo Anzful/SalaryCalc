a08e1d2b2d5195be8248cd48a7373c5ef40bab21
Anzo <2003anzori@gmail.com>
2023-04-24 18:08:37 +0400

    *

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
-const working_hours = document.getElementById('working_hours');
-const sgc = document.getElementById('sgc');
-const reaction_time = document.getElementById('reaction_time');
-const pa = document.getElementById('pa');
-const game_speed = document.getElementById('game_speed');
-const button = document.getElementById('button');
-const stage = document.getElementById('stage');
-const shift = document.getElementById('shift');
+const A = document.getElementById('working_hours'); //working hours
+const B = document.getElementById('sgc');           //sgc
+const C = document.getElementById('reaction_time'); //reaction time
+const D = document.getElementById('pa');            //pa
+const E = document.getElementById('game_speed');    //game speed
+const F = document.getElementById('button');        //button
+const G = document.getElementById('stage');         //stage
+const H = document.getElementById('shift');         //shift
+const I = document.getElementById('roulette_cooperation')//roulette cooperation
 
 button.addEventListener('click', (ev)=>{
     
-    alert(parseInt(working_hours.value))
+    working_hours = parseFloat(A.value)
+    sgc = parseInt(B.value)
+    reaction_time = parseFloat(C.value)
+    pa = parseFloat(D.value)
+    game_speed = parseFloat(E.value)
+    roulette_cooperation = parseFloat(I.value)
+    button = F.value
+    stage = G.value
+    shift = H.value
+    
+    if(stage='stage 1'){
+        if(shift='morning'){
+
+        }
+        
+        else if(shift='night'){
+
+        }
+    }
+
+
+    else if(stage='stage 2'){
+        if(shift='morning'){
+
+        }
+
+        else if(shift='night'){
+            
+        }
+    }
+
+
+    else if(stage='stage 3'){
+        if(shift='morning'){
+
+        }
+
+        else if(shift='night'){
+            
+        }
+    }
+    
+    
 
 })
 
